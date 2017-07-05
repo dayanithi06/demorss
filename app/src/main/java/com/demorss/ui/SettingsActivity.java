32f3258dc2779dc63f8e02094832706e0237c641
dayanithi06 <android.daya@gmail.com>
2017-07-05 14:38:05 +0530

    setting id created reddit and yahoo

--- app/src/main/java/com/demorss/ui/SettingsActivity.java
+++ app/src/main/java/com/demorss/ui/SettingsActivity.java
@@ -1,10 +1,25 @@
 package com.demorss.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.demorss.BaseActivity;
+import com.demorss.R;
 
 /**
  * Created by DAYANITHI on .04-07-2017.
  */
 
 public class SettingsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        initViews();
+
+    }
+
+    private void initViews() {
+
+    }
 }
