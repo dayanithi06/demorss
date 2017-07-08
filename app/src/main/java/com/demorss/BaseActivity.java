91cda6dbc4f066d86c9cb55acb8c41718d120e64
dayanithi06 <android.daya@gmail.com>
2017-07-08 14:43:12 +0530

    timer added

--- app/src/main/java/com/demorss/BaseActivity.java
+++ app/src/main/java/com/demorss/BaseActivity.java
@@ -1,9 +1,12 @@
 package com.demorss;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import com.demorss.service.TimerService;
 
 /**
  * Created by DAYANITHI on. 04-07-2017.
@@ -16,7 +19,13 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
     }
 
-    public void showToast(String mess){
-        Toast.makeText(this,mess,Toast.LENGTH_SHORT).show();
+    public void showToast(String mess) {
+        Toast.makeText(this, mess, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(this, TimerService.class));
     }
 }
